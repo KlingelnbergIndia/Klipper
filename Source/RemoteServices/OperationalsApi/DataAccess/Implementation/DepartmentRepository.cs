4d6eb89360af0b9cfbe9d85e3eff2cc594c856ca
Kiran Kharade <kharade_kiran@yahoo.com>
2018-11-29 16:02:22 +0530

    Added code to OperationalsApi.

--- Source/RemoteServices/OperationalsApi/DataAccess/Implementation/DepartmentRepository.cs
+++ Source/RemoteServices/OperationalsApi/DataAccess/Implementation/DepartmentRepository.cs
@@ -1,4 +1,4 @@
-﻿using EmployeeApi.DataAccess.Interfaces;
+﻿using OperationalsApi.DataAccess.Interfaces;
 using Models.Core.Operationals;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
@@ -7,15 +7,15 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace EmployeeApi.DataAccess.Implementation
+namespace OperationalsApi.DataAccess.Implementation
 {
     public class DepartmentRepository : IDepartmentRepository
     {
-        private readonly EmployeeDBContext _context = null;
+        private readonly DepartmentDBContext _context = null;
 
         public DepartmentRepository()
         {
-            _context = EmployeeDBContext.Instance;
+            _context = DepartmentDBContext.Instance;
         }
 
         public async Task AddDepartment(Department item)
